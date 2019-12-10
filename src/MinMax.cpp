9e730cf722842b5f30efda94014221f798280e2e
bobbywan-kenoby <leonoixdecoco@gmail.com>
2019-12-10 18:46:15 +0100

    end of day

--- src/MinMax.cpp
+++ src/MinMax.cpp
@@ -1,8 +1,23 @@
 #include "MinMax.hpp"
 
 MinMax::MinMax(symbole signe, Board* board) : IComputer(signe, board){
+  //creation des threads
 }
 
 void MinMax::algorithm(int& grid, int& cell) {
 
+}
+
+void MinMax::heuristic(){
+
+}
+
+void MinMax::funcThread(){
+  //fonction pour les threads
+
+  //boucle tant que la partie existe
+  //attendre les taches a faire depuis la pile
+  //get une tache puis la suppr
+  //si feuille alors heuristic sinon min/max resultat des enfants
+  //mettre le resultat de la tache dans l'emplacement memoire dedié
 }
--- src/MinMax.hpp
+++ src/MinMax.hpp
@@ -1,16 +1,16 @@
 #ifndef MINMAX_HPP
 #define MINMAX_HPP
 
-
+#include "IComputer.hpp"
 
 class MinMax : public IComputer {
 private:
   void algorithm(int& grid, int& cell);
   void heuristic();
+  void funcThread();
 
 public:
   MinMax(symbole signe, Board* board);
 };
-
 
 #endif /* end of include guard: MINMAX_HPP */
