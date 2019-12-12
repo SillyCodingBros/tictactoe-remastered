33d1a6d550ff0f0315db47734ede468a647b3488
bobbywan-kenoby <leonoixdecoco@gmail.com>
2019-12-12 17:46:02 +0100

    Board.hpp : add possibilities

--- src/Board.hpp
+++ src/Board.hpp
@@ -10,6 +10,14 @@ private:
   symbole board_[81];
   int curGrid_;
   symbole winner_;
+  int POSSIBILITIES[24] = {0, 1, 2,
+                           3, 4, 5,
+                           6, 7, 8,
+                           0, 3, 6,
+                           1, 4, 7,
+                           2, 5, 8,
+                           0, 4, 8,
+                           2, 4, 6};
 
 public:
   Board();
